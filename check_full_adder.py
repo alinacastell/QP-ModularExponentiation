f48f1b67287a48dc47e20941a35a0835314af41a
alinacastell <alinacastell@estudiantat.upc.edu>
2025-01-17 11:39:01 +0100

    simplified full adder

--- check_full_adder.py
+++ check_full_adder.py
@@ -4,28 +4,6 @@ from functions import *
 from utils import print_measures
 from qiskit import QuantumCircuit
 
-# Full Adder
-def full_adder(circuit, a, b, r, c_in, c_out, AUX):
-    '''
-    Implement a full adder.
-    '''
-    # r = a xor b xor c_in
-    circuit.cx(a,r)
-    circuit.cx(b,r)
-    circuit.cx(c_in,r)
-    # AUX[0] = a AND b
-    circuit.ccx(a, b, AUX[0])
-    # AUX[1] = a xor b
-    circuit.cx(a,AUX[1])
-    circuit.cx(b,AUX[1])
-    # AUX[2] = c_in AND (a xor b)
-    circuit.ccx(c_in,AUX[1],AUX[2])
-    # NOT AUX[0] and NOT AUX[2]
-    circuit.x(AUX[0])
-    circuit.x(AUX[2])
-    # c_out = AUX[0] OR AUX[2]
-    circuit.ccx(AUX[0], AUX[2], c_out)
-    circuit.x(c_out)
 
 # Code check full adder
 a = 0
