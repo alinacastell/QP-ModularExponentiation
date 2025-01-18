fbe59e168a5a6b449e59aba4cd8bf031390fbd64
alinacastell <alinacastell@estudiantat.upc.edu>
2025-01-18 17:26:17 +0100

    tested checks

--- check_subtraction.py
+++ check_subtraction.py
@@ -15,18 +15,39 @@ def subtract(circuit, A, B, R, AUX):
     circuit.x(AUX[0])
     add(circuit, A, B, R, AUX)
     # Reset carry-in bit to 0
-    circuit.x(AUX[0])
+    #circuit.x(AUX[0])
 
-# Code check subtraction
+# Code check 2-qubit subtraction
+'''
+A = [0,1]
+B = [2,3]
+AUX = [4,5,6,9,10,11]
+R = [7,8]
+n = len(A) + len(B) + len(R) + len(AUX)
+circuit = QuantumCircuit(n, 3)
+print("\nSubtraction Circuit\n")
+print("A = [a0,a1] = [1,1] // B = [b0,b1] = [1,0]\n")
+set_bits(circuit, R, [0,0,0])
+set_bits(circuit, A, [1,1])
+set_bits(circuit, B, [1,1])
+subtract(circuit, A, B, R, AUX)
+circuit.barrier()
+circuit.measure([AUX[2],R[1],R[0]], [0,1,2])
+print_measures(circuit)
+'''
+# Code check 3-qubit subtraction
+
 A = [0,1,2]
 B = [3,4,5]
 R = [6,7,8]
-AUX = [9,10,11,12, 13, 14,15,16]
+AUX = [9,10,11,12, 13, 14,15]
 n = len(A) + len(B) + len(R) + len(AUX) + 4
 circuit = QuantumCircuit(n,len(R))
-set_bits(circuit, [A, B], [[1,0,0],[1,1,0]])
+set_bits(circuit, A, [1,0,0])
+set_bits(circuit, B,[1,1,0])
 subtract(circuit, A, B, R, AUX)
 print("\nSubtraction Circuit\n")
+print("A = [a0,a1,a2] = [1,0,0] // B = [b0,b1,b2] = [1,1,0]\n")
 circuit.barrier()
 circuit.measure(R, [0,1,2])
 print_measures(circuit)
